b3b0d42afb9d707ce42080eb13d6fd888196c93c
Thet Htet Aung <thethtetaung17@gmail.com>
2024-05-16 19:04:40 +0800

    Event gateway integration with message send and receive

--- apps/gateway/src/gateway.gateway.ts
+++ apps/gateway/src/gateway.gateway.ts
@@ -1,23 +1,53 @@
-import { Inject } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Inject, Logger } from '@nestjs/common';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { GatewayService } from './gateway.service';
 
 @WebSocketGateway()
-export class GatewayGateway {
+export class GatewayGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    protected readonly logger = new Logger(GatewayGateway.name);
+    @WebSocketServer()
+    server: Server;
 
-//   @WebSocketServer()
-//   server: Server;
+    clients: Socket[] = [];
+    constructor(@Inject(GatewayService) private readonly gatewayService: GatewayService) { }
 
-//   constructor(@Inject(GatewayService) private readonly gatewayService: GatewayService) { }
+    emitNewMessage(payload: Record<string, any>) {
+        // this.server.emit()
+    }
 
-//   // @SubscribeMessage('send-message')
-//   // handleMessage(client: any, payload: any): string {
-//   //   return 'Hello world!';
-//   // }
+    afterInit(server: any) {
+        this.logger.log(`Socket initialzed @ ${new Date().toString()}`);
+    }
 
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+    handleConnection(client: Socket, ...args: any[]) {
+        this.clients.push(client);
+        this.logger.log(`Client with id ${client?.id} connected @ ${new Date().toString()}`);
+        this.logger.log('Client IDs:', this.clients.map(client => client.id))
+    }
+
+    handleDisconnect(client: Socket) {
+        this.clients = this.clients.filter(c => c.id !== client.id);
+        this.logger.log(`Client with id ${client?.id} disconnected @ ${new Date().toString()}`);
+        this.logger.log('Client IDs:', this.clients.map(client => client.id))
+    }
+
+    @SubscribeMessage('message')
+    async handleMessage(client: Socket, payload: any) {
+        const result = this.gatewayService.sendMessage(client, payload);
+        this.logger.log('New Message Result:', result);
+        this.emitToOtherClients(client, payload);
+    }
+
+    emitToOtherClients(sender: Socket, payload) {
+        this.logger.log('Emitting Client IDs:', this.clients.map(client => client.id))
+
+        for (let client of this.clients) {
+            if (sender.id !== client.id) {
+
+                client.emit('new-message', payload);
+                this.logger.log(`Emitted to client '${client.id}'`)
+            }
+        }
+    }
 }
